6449ffc796beb6cc119d2f09153469b69b2ad95e
Alan Rizky Wardana <androidmaniac28@gmail.com>
2019-10-21 15:17:20 +0700

    RecycleviewGambarDanText

---
+++ RecycleviewGambarDanText/app/src/main/java/com/alan/recycleviewgambardantext/models/SuperHero.java
@@ -0,0 +1,41 @@
+package com.alan.recycleviewgambardantext.models;
+
+import android.widget.ImageView;
+
+import com.alan.recycleviewgambardantext.R;
+
+public class SuperHero {
+    public String heroName;
+    public String heroText;
+    public int heroImage;
+
+    public SuperHero(String heroName, String heroText, int heroImage) {
+        this.heroName = heroName;
+        this.heroText = heroText;
+        this.heroImage = heroImage;
+    }
+
+    public String getHeroName() {
+        return heroName;
+    }
+
+    public void setHeroName(String heroName) {
+        this.heroName = heroName;
+    }
+
+    public String getHeroText() {
+        return heroText;
+    }
+
+    public void setHeroText(String heroText) {
+        this.heroText = heroText;
+    }
+
+    public int getHeroImage() {
+        return heroImage;
+    }
+
+    public void setHeroImage(int heroImage) {
+        this.heroImage = heroImage;
+    }
+}
