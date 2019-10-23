4fc62f0b096db51cef9f14de0fc3ce7a1e2c8c34
Alan Rizky Wardana <androidmaniac28@gmail.com>
2019-10-23 13:07:28 +0700

    RecycleviewGambarDanTextSertaScrollMenggunakanGridLayoutManager

---
+++ RecycleviewGambarDanTextSertaScrollMenggunakanGridLayoutManager/app/src/main/java/com/alan/recycleviewgambardantextsertascrollmenggunakangridlayoutmanager/models/SuperHero.java
@@ -0,0 +1,28 @@
+package com.alan.recycleviewgambardantextsertascrollmenggunakangridlayoutmanager.models;
+
+public class SuperHero {
+    public String heroName;
+    public String heroText;
+    public int heroImage;
+
+    public SuperHero(String heroName, String heroText, int heroImage) {
+        this.heroName = heroName;
+        this.heroText = heroText;
+        this.heroImage = heroImage;
+    }
+
+    public String getHeroName() {
+        return heroName;
+    }
+
+
+    public String getHeroText() {
+        return heroText;
+    }
+
+
+    public int getHeroImage() {
+        return heroImage;
+    }
+
+}
